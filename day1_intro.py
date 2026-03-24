9526324cac4ff4a3437d12a4b4a2f8811000dc61
bhairavi <bhairavi200623@gmail.com>
2026-03-24 16:32:59 +0530

    Refactor day1_intro.py for better structure

--- day1_intro.py
+++ day1_intro.py
@@ -1,11 +1,20 @@
+#input
 name ="butterfly"
-age = 19
-
 print("my name is",name)
+
+#numbers
+age = 19
 print("my age is",age)
 
+#print funtion() 
 city ="pune"
 food ="pani puri"
-
 print("i live in",city)
 print("my favoutrie food is",food)
+
+print('       1')
+print('     2 3')
+print('   4 5 6')
+print('7 8 9 10')
+
+
