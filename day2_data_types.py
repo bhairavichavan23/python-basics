77f00860089eebae4ceb4f64e44716bb3415469c
bhairavi <bhairavi200623@gmail.com>
2026-03-26 16:58:56 +0530

    DAY 2 : practeiced data types , type conversion , string operation and boolean logic
    Added comments for clarity on data types and conversions.

--- day2_data_types.py
+++ day2_data_types.py
@@ -10,12 +10,17 @@ print(type(b))
 print(type(c))
 print(type(d))
 
+#types of conversion 
 x=5
 y='3'
 z= 2
-
-print(x+z) 
-print(int(y)+x)
-print(y*x)
-print(x>z)
-print(y+str(z))
+# integer + integer = sum
+print(x+z) #7
+# string converted to integer + integer = sum 
+print(int(y)+x) #8
+#string * integer = repeat
+print(y*x) # 33333
+#boolean
+print(x>z) #TRUE
+#string + integer converted to string = join 
+print(y+str(z))  #32
