496d57daed12bd5fa8b15e38ffd45784d3b54ac3
bhairavi <bhairavi200623@gmail.com>
2026-03-26 16:49:34 +0530

    Add files via upload

---
+++ day2_data_types.py
@@ -0,0 +1,21 @@
+# types of data
+
+a=9
+b=8.9
+c='hi'
+d=False
+
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+
+x=5
+y='3'
+z= 2
+
+print(x+z) 
+print(int(y)+x)
+print(y*x)
+print(x>z)
+print(y+str(z))
